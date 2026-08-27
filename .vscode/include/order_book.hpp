ad7ce188247cc3e036c4c004e5df1a3fbce2be51
arjunerasani <aerasani@purdue.edu>
2026-08-26 20:11:52 -0400

    coded the cancelOrder method which required a seperate map to keep track of order locations to ensure O(1) lookup

--- .vscode/include/order_book.hpp
+++ .vscode/include/order_book.hpp
@@ -3,15 +3,31 @@
 
 // map is a red black tree under the hood so it keeps the keys automatically sorted, which is what we want for the order book
 #include <map>
+#include <unordered_map>
 #include <list>
+
+struct orderLocation {
+    orderType orderType;
+    uint64_t price;
+
+    // can't just have a normal iterator because the iterator needs to now what it is iterating over
+    // this is just 1 value btw
+    std::list<order>::iterator iterator; // this is a pointer to the node inside the list
+};
 
 // this file represents the order book, which is a collection of price levels for both buy and sell orders
 // the bids are to be sorted in descending order, while the asks are to be sorted in ascending order
 // using a class here because we want to encapsulate the data and operations for the order book, and we want to be able to create multiple instances of the order book if needed
 class orderBook {
 private:
+    // in 8 byte integer is the data type for the key (price) and the priceLevel data type is for the value (priceLevel)
     std::map<uint64_t, priceLevel, std::greater<uint64_t>> bids; // descending order for bids
     std::map<uint64_t, priceLevel> asks; // ascending order for asks
+
+    // the 8 byte integer here is for the key data type (orderId) and the orderLocation data type is for the value (orderLocation)
+    // an unordered map is used because cancelling orders requires many "layers" to be explored leading to a large time complexity
+    // this makes lookup O(1)
+    std::unordered_map<uint64_t, orderLocation> orderIndex;
 
 public:
     orderBook(std::map<uint64_t, priceLevel, std::greater<uint64_t>> b, std::map<uint64_t, priceLevel> a) {
@@ -20,6 +36,8 @@ public:
     }
 
     void addOrder(order& newOrder) {
+        std::list<order>::iterator orderIterator;
+
         if (newOrder.orderType == orderType::BUY) {
             // if bids.find returns the end of the map then it doesn't exist because
             // bids.end() is the theoretical 1 space past the last element in the map
@@ -28,24 +46,61 @@ public:
 
                 // this means that a new key is created in bids (price) and a new value is assigned to the key (newPriceLevel)
                 bids[newOrder.price] = newPriceLevel;
+
+                // this gets the last order added and marks its location in the unordered map using an iterator
+                orderIterator = std::prev(newPriceLevel.orders.end());
             } else {
                 // this finds the existing price level by using the key and then adds the new order
                 // to the back of the price level
                 priceLevel existingPriceLevel = bids[newOrder.price];
                 existingPriceLevel.appendOrderToBack(existingPriceLevel, newOrder);
+
+                orderIterator = std::prev(existingPriceLevel.orders.end());
             }
+
+            // this maps the orderId to the orderLocation which contains the iterator we got above
+            orderIndex[newOrder.orderId] = orderLocation{orderType::BUY, newOrder.price, orderIterator};
         } else {
             if (asks.find(newOrder.price) == asks.end()) {
                 priceLevel newPriceLevel = priceLevel{.price = newOrder.price, .orders = std::list<order>{newOrder}};
                 asks[newOrder.price] = newPriceLevel;
+
+                orderIterator = std::prev(newPriceLevel.orders.end());
             } else {
                 priceLevel existingPriceLevel = asks[newOrder.price];
                 existingPriceLevel.appendOrderToBack(existingPriceLevel, newOrder);
+
+                orderIterator = std::prev(existingPriceLevel.orders.end());
             }
+
+            orderIndex[newOrder.orderId] = orderLocation{orderType::SELL, newOrder.price, orderIterator};
         }
     }
 
     void cancelOrder(uint64_t orderId) {
-        
+        // this returns a map pair with ->first holding the key and ->second holding the value
+        auto mapIt = orderIndex.find(orderId);
+
+        if (mapIt == orderIndex.end()) {
+            std::cerr << "Error: Attempted to cancel an order that does not exist." << std::endl;
+            return;
+        } else {
+            // use a reference here so it isn't a copy
+            orderLocation& cancelledOrder = mapIt->second;
+
+            if (cancelledOrder.orderType == orderType::BUY) {
+                // have to erase the cancelled order from both the priceLevel.orders list its in and the unordered map
+                // use a reference so the actual priceLevel is modified
+                priceLevel& cancelledLevel = bids[cancelledOrder.price];
+
+                cancelledLevel.orders.erase(cancelledOrder.iterator);
+            } else {
+                priceLevel& cancelledLevel = asks[cancelledOrder.price];
+
+                cancelledLevel.orders.erase(cancelledOrder.iterator);
+            }
+
+            orderIndex.erase(mapIt);
+        }
     }
 };
